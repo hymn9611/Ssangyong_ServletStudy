8c7d6d2907687c61c078b2a2f194c6349c25ea0e
Hyunmin Choi <74778827+hymn9611@users.noreply.github.com>
2021-08-26 10:36:50 +0900

    commit

---
+++ src/main/java/java/servlet/s1/FrontController.java
@@ -0,0 +1,26 @@
+package java.servlet.s1;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FrontController")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public FrontController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("FrontController 실행");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
